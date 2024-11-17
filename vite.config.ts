eee83fb1775a426813271bada230ef31b3c92465
vigourpt <vigourpt@googlemail.com>
2024-11-17 16:55:51 +0000

    Update vite.config.ts for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/affiliate-marketing-calculator/', // Set this to the subdirectory path
+  base: './', // Use relative path
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name][extname]',
+        entryFileNames: 'index.js',
+        chunkFileNames: 'index.js',
+        assetFileNames: 'index[extname]',
       },
     },
   },
