4d6c99066916ee2e6df0eaef13f91304aeee8666
vigourpt <vigourpt@googlemail.com>
2024-11-17 13:47:26 +0000

    Update build with correct base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/affiliate-marketing-calculator/', // Set the base path for the subdirectory
+  base: '/affiliate-marketing-calculator/', // Set this to the subdirectory path
   plugins: [react()],
 });
